24889521ae07bdf37315ce60b9be0726c25e96fb
isacleibe <isac.leibe@gmail.com>
2019-03-22 02:37:37 +0100

    Includes D6 and D12 chars.
    Incluye los caracteres del D6 y del D12. Necesita depuración en la
    seleccion del caracter a mostrar por el display. Necesita reducir
    tamaño matriz de la librería (tamaño excesivo). Usa dos botones
    en los pines 2 y 3, resistencias de pull-down de 10kOhms.

--- 8x8-led-matrix-dice/8x8-led-matrix-dice.ino
+++ 8x8-led-matrix-dice/8x8-led-matrix-dice.ino
@@ -5,7 +5,8 @@ LedControl lc=LedControl(12,11,10, NumMatrix);  // Creamos una instancia de LedC
 
 void setup()
 {
-  pinMode(2,INPUT);
+  pinMode(2,INPUT);//D6
+  pinMode(3,INPUT);//D12
   for (int i=0; i< NumMatrix ; i++){
     lc.shutdown(i,false);    // Activar las matrices
     lc.setIntensity(i,1);    // Poner el brillo a un valor intermedio
@@ -15,14 +16,18 @@ void setup()
 
 void loop()
 {
-  lc.displayChar(0,46);
-  /*
   if (digitalRead(2)==HIGH) {
-    for (size_t i = 0; i < 15; i++) {      
-      int R = random(1,7);
-      lc.displayChar(0,R+40);
-      delay(100);
+    for (size_t i = 0; i < 15; i++) {
+      int R = random(1,7);//7 excluyente
+      lc.displayChar(0,R+30);
+      delay(90);
     }
   }
-  */
+  if (digitalRead(3)==HIGH) {
+    for (size_t i = 0; i < 15; i++) {
+      int R = random(1,14);//13 excluyente, la pos 0 coincide con 36
+      lc.displayChar(0,R+36);
+      delay(90);
+    }
+  }
 }
